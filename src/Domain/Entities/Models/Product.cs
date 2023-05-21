09b17ba8619ef072b4e16a50e62afdf6889dd2ed
João Victor de Souza <j.souzalimasantos@gmail.com>
2023-05-21 18:33:25 -0300

    feat: update 'Product' entity class

--- src/Domain/Entities/Models/Product.cs
+++ src/Domain/Entities/Models/Product.cs
@@ -1,20 +1,31 @@
-﻿namespace Domain.Entities.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Domain.Entities.Models
 {
     public sealed class Product
     {
         /// <summary>
+        /// Unique identificator of a product
+        /// </summary>
+        [Key]
+        public Guid Id { get; set; }
+
+        /// <summary>
         /// Name of the product
         /// </summary>
+        [Required]
         public string Name { get; set; }
 
         /// <summary>
         /// Quantity of the product in stock
         /// </summary>
+        [Required]
         public int Stock { get; set; }
 
         /// <summary>
         /// Value of the product
         /// </summary>
+        [Required]
         public double Value { get; set; }
     }
 }
