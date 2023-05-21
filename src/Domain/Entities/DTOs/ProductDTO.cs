21acd2a50062cb1906231d192b47851b3c98211d
João Victor de Souza <j.souzalimasantos@gmail.com>
2023-05-21 18:30:13 -0300

    feat: create the entities 'Product' and 'ProductDTO'

---
+++ src/Domain/Entities/DTOs/ProductDTO.cs
@@ -0,0 +1,20 @@
+﻿namespace Domain.Entities.DTOs
+{
+    public sealed class ProductDTO
+    {
+        /// <summary>
+        /// Name of the product
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Quantity of the product in stock
+        /// </summary>
+        public int Stock { get; set; }
+
+        /// <summary>
+        /// Value of the product
+        /// </summary>
+        public double Value { get; set; }
+    }
+}
---
+++ src/Domain/Entities/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace Domain.Entities.Models
+{
+    public sealed class Product
+    {
+        /// <summary>
+        /// Name of the product
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Quantity of the product in stock
+        /// </summary>
+        public int Stock { get; set; }
+
+        /// <summary>
+        /// Value of the product
+        /// </summary>
+        public double Value { get; set; }
+    }
+}
