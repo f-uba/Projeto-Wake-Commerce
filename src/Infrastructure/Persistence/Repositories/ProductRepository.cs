3408d97327b14dcf592fc13d087bd716d6fcf0a1
João Victor de Souza <j.souzalimasantos@gmail.com>
2023-05-21 23:31:34 -0300

    feat: create the repositories classes

---
+++ src/Infrastructure/Persistence/Repositories/ProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using Application.Interfaces;
+using Domain.Entities.Models;
+using Infrastructure.Persistence.Context;
+
+namespace Infrastructure.Persistence.Repositories
+{
+    internal sealed class ProductRepository : Repository<Product>, IProductRepository
+    {
+        public ProductRepository(AppDbContext context) : base(context) { }
+
+        public Product? GetById(Guid id)
+        {
+            return Get().FirstOrDefault(product => product.Id == id);
+        }
+
+        public void DeleteById(Guid id)
+        {
+            var product = GetById(id);
+            Delete(product);
+        }
+    }
+}
---
+++ src/Infrastructure/Persistence/Repositories/Repository.cs
@@ -0,0 +1,37 @@
+﻿using Application.Interfaces;
+using Infrastructure.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence.Repositories
+{
+    internal class Repository<T> : IRepository<T> where T : class
+    {
+        protected AppDbContext _context;
+
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(T entity)
+        {
+            _context.Set<T>().Add(entity);
+        }
+
+        public void Delete(T entity)
+        {
+            _context.Set<T>().Remove(entity);
+        }
+
+        public IQueryable<T> Get()
+        {
+            return _context.Set<T>().AsNoTracking();
+        }
+
+        public void Update(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            _context.Set<T>().Update(entity);
+        }
+    }
+}
