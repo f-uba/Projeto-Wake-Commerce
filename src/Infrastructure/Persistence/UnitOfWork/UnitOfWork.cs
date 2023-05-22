b486974b3e625c255e955b818ef8bf7dc6922b40
João Victor de Souza <j.souzalimasantos@gmail.com>
2023-05-21 23:32:25 -0300

    feat: create the UnitOfWork class

---
+++ src/Infrastructure/Persistence/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using Application.Interfaces;
+using Infrastructure.Persistence.Context;
+using Infrastructure.Persistence.Repositories;
+
+namespace Infrastructure.Persistence.UnitOfWork
+{
+    public sealed class UnitOfWork : IUnitOfWork
+    {
+        public AppDbContext _context;
+
+        public IProductRepository _productRepository;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IProductRepository ProductRepository => _productRepository ??= new ProductRepository(_context);
+
+        public async Task Commit()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
