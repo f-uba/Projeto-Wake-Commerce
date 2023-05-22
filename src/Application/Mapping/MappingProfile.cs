1361ee07bca57b8f4f69cfc60a1c3141e52fdd93
João Victor de Souza <j.souzalimasantos@gmail.com>
2023-05-21 23:30:01 -0300

    feat: create the mapper class

---
+++ src/Application/Mapping/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Domain.Entities.DTOs;
+using Domain.Entities.Models;
+
+namespace Application.Mapping
+{
+    public sealed class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Product, ProductDTO>().ReverseMap();
+        }
+    }
+}
