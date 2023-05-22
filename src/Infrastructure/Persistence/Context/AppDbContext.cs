0ecaa47be24f04ef9a6c7740668f86ebba456cb5
João Victor de Souza <j.souzalimasantos@gmail.com>
2023-05-21 23:32:09 -0300

    feat: create the AppDbContext class

---
+++ src/Infrastructure/Persistence/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence.Context
+{
+    public sealed class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>();
+        }
+    }
+}
